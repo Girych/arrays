27836e6a7feaa0c52ad60c290c3ff2642913698b
Girych <dmitriev.ilya@mail.ru>
2022-07-05 17:39:33 +0300

    codeFix

--- src/main/java/StatsService.java
+++ src/main/java/StatsService.java
@@ -3,50 +3,53 @@ public class StatsService {
         long sum = 0;
         for (long sale : sales) {
             sum = sum + sale;
-
         }
         return sum;
-
     }
+
     public long average(long[] sales) {
         //long sum = sum(sales);
         return sum(sales) / sales.length;
     }
-    public int maxSalesMonth(long[] sales){
+
+    public int maxSalesMonth(long[] sales) {
         int maxMonth = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] >= sales[maxMonth]){
+            if (sales[i] >= sales[maxMonth]) {
                 maxMonth = i;
             }
         }
         return maxMonth;
     }
-    public int minSalesMonth(long[] sales){
+
+    public int minSalesMonth(long[] sales) {
         int minMonth = 0;
         int month = 0;
         for (long sale : sales) {
-            if (sale <= sales[minMonth]){
+            if (sale <= sales[minMonth]) {
                 minMonth = month;
             }
             month = month + 1;
         }
         return minMonth + 1;
     }
-    public int belowAvg(long[] sales){
+
+    public int belowAvg(long[] sales) {
         long avg = average(sales);
-                int count = 0;
-        for (long sale : sales){
-            if (sale < avg){
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < avg) {
                 count++;
             }
         }
-    return count;
+        return count;
     }
-    public int aboveAvg(long[] sales){
+
+    public int aboveAvg(long[] sales) {
         long avg = average(sales);
         int count = 0;
-        for (long sale : sales){
-            if (sale > avg){
+        for (long sale : sales) {
+            if (sale > avg) {
                 count++;
             }
         }
